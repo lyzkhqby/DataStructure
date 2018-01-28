4f55fdb73e15f27f2448e55c294c4a2715ab5c39
lyzkhqby <lyzkhqby@sina.com>
2018-01-29 03:05:05 +0800

    default

---
+++ src/com/zk/queue/LLQueue.java
@@ -0,0 +1,21 @@
+package com.zk.queue;
+
+import com.zk.tree.BinaryTreeNode;
+
+public class LLQueue {
+    public void enQueue(BinaryTreeNode root) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public BinaryTreeNode deQueue() {
+        return null;
+    }
+
+    public void deleteQueue() {
+
+    }
+}
--- src/com/zk/tree/Traversal.java
+++ src/com/zk/tree/Traversal.java
@@ -1,5 +1,6 @@
 package com.zk.tree;
 
+import com.zk.queue.LLQueue;
 import com.zk.stack.LLStack;
 
 public class Traversal {
@@ -78,4 +79,63 @@ public class Traversal {
             }
         }
     }
+
+    public void levelOrder(BinaryTreeNode root) {
+        BinaryTreeNode temp;
+        LLQueue Q = new LLQueue();
+        if (root == null) return;
+        Q.enQueue(root);
+        while (!Q.isEmpty()) {
+            temp = Q.deQueue();
+            System.out.println(temp.getData());
+            if (temp.getLeft() != null) {
+                Q.enQueue(temp.getLeft());
+            }
+            if (temp.getRight() != null) {
+                Q.enQueue(temp.getRight());
+            }
+        }
+        Q.deleteQueue();
+    }
+
+    public int findMax(BinaryTreeNode root) {
+        int root_val, left, right, max = Integer.MIN_VALUE;
+        while (root != null) {
+            root_val = root.getData();
+            left = findMax(root.getLeft());
+            right = findMax(root.getRight());
+            if (left > right) {
+                max = left;
+            }else {
+                max = right;
+            }
+            if (root_val > max) {
+                max = root_val;
+            }
+        }
+        return max;
+    }
+
+    public int findMaxUsingLevelOrder(BinaryTreeNode root) {
+        BinaryTreeNode temp;
+        int max = Integer.MIN_VALUE;
+        LLQueue Q = new LLQueue();
+        Q.enQueue(root);
+        while (!Q.isEmpty()) {
+            temp = Q.deQueue();
+            if (max < temp.getData()) {
+                max = temp.getData();
+            }
+            if (temp.getLeft() != null) {
+                Q.enQueue(temp.getLeft());
+            }
+            if (temp.getRight() != null) {
+                Q.enQueue(temp.getRight());
+            }
+        }
+        Q.deleteQueue();
+        return max;
+    }
+
+
 }
